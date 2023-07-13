792d9aa5106e6221b11fd38c05d60c9c4c8bc0fb
Liridona Muhaxheri <lm28887@seeu.edu.mk>
2023-07-13 05:02:23 +0200

    starting the project

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+import "../style.css";
+export default function Main (){
+    return (
+        <div className="main">
+            <h1>This is a testtt</h1>
+        </div>
+    )
+}
